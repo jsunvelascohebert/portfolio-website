a19ccdc4e5d66c735db180707d603f0cf7cb737c
jsunvelascohebert <jsun.velasco.hebert@gmail.com>
2023-11-17 16:43:38 -0500

    finick with some home page elements

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,14 +8,23 @@ export default function Home() {
     <section id="home" className="w-full min-h-screen flex flex-col gap-10">
 
       {/* hero banner */}
-      <div className='w-full h-[90vh] flex flex-col md:flex-row justify-center md:justify-around items-center p-10 gap-5 md:gap-0 bg-gradient-to-b from-purple to-white'>
+      <div className='w-full h-[90vh] flex flex-col md:flex-row justify-center md:justify-center items-center p-10 gap-10 md:gap-20 bg-gradient-to-b from-purple to-white'>
         <Fade>
         {/* image */}
         <img src={require("./../assets/jvh_headshot.png")} alt="jsun headshot" className='w-[50vw] md:w-[25vw] object-cover rounded-full border-4 border-darkPurple'/>
         {/* name */}
         <div className="flex flex-col justify-start items-center gap-3">
-          <h1 className='scale-75'>jsun velasco-hébert</h1>
-          <p className='text-light italic text-darkPurple'>designer  |  educator  |  developer</p>
+          <h1 className='text-2xl md:text-6xl'>jsun velasco-hébert</h1>
+            <div className="w-full flex flex-row justify-center items-center gap-2 sm:gap-5">
+              <p className="text-light italic text-darkPurple">designer</p>
+              <p className="text-light text-darkPurple">|</p>
+              <p className="text-light italic text-darkPurple">developer</p>
+              <p className="text-light text-darkPurple">|</p>
+              <p className="text-light italic text-darkPurple">educator</p>
+
+
+            </div>  
+     
           </div>
           </Fade>
       </div>
