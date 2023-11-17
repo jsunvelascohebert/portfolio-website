52331331cd410f21c4cb65c7841bdc8ff415aa0a
jsunvelascohebert <jsun.velasco.hebert@gmail.com>
2023-11-17 10:26:44 -0500

    save point

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,22 +3,27 @@ import React from 'react';
 export default function Home() {
   return (<>
     {/* general container */}
-    <div className="w-full bg-lightPurple min-h-screen">
+    <section id="home" className="w-full bg-lightPurple min-h-screen flex flex-col gap-10">
       {/* hero banner */}
       <div className='flex flex-col md:flex-row justify-center md:justify-around items-center'>
         {/* image */}
-        <img src="../assets/jvh_headshot.png" alt="jsun headshot" />
+        <img src="./../assets/jvh_headshot.png" alt="jsun headshot" />
         {/* name */}
-        <div className="flex flex-col justify-start items-start gap-3">
+        <div className="flex flex-col justify-start items-center gap-3">
           <h1>jsun velasco-hebert</h1>
           <p>designer  |  educator  |  developer</p>
         </div>
       </div>
 
-      {/* basic information */}
-
       {/* quick link to projects */}
+      <div className='w-full bg-white border-2 border-darkPurple flex flex-col justify-center items-center gap-2 p-4'>
+        <h2>projects</h2>
 
-    </div>
+        {/* project gallery */}
+        
+
+      </div>
+
+    </section>
   </>);
 }
