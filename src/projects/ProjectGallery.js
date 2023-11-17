df9d4ca66e06d0b3ff704cfb8221b622808a64ca
jsunvelascohebert <jsun.velasco.hebert@gmail.com>
2023-11-17 17:04:32 -0500

    replace some elements in project gallery

--- src/projects/ProjectGallery.js
+++ src/projects/ProjectGallery.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Fade } from 'react-awesome-reveal';
 
 export default function ProjectGallery() {
   return (<>
@@ -7,7 +8,7 @@ export default function ProjectGallery() {
     <div className="w-full flex flex-wrap justify-center items-stretch border-2 border-darkPurple">
 
       {/* medipin card */}
-      <div className="project-card" tabIndex={0}>
+      <Link className='project-card'>
         {/* image */}
         <img src={require("./thumbnails/medipin-card-thumbnail.png")} alt="" className='project-card-img'/>
         {/* title */}
@@ -16,10 +17,10 @@ export default function ProjectGallery() {
         <p className="project-card-date">Fall 2023</p>
         {/* description */}
         <p>In order to bridge the gap in knowledge between medical information and day-to-day awareness, Medipin was created as a proof-of-concept, pin-and-note web application. With Medipin, users can create an account, search and save medical articles from an external API, organize articles based on customized topics, and add personalized notes to each article. This project was solo-coded by me; moreover, enhancements of the site will continue to come out when time permits.</p>
-        </div>
-
+      </Link>
+      
       {/* storyframing card */}
-      <div className="project-card" tabIndex={0}>
+      <Link className="project-card">
         {/* image */}
         <img src={require("./thumbnails/storyframing-card-thumbnail.png")} alt="" className='project-card-img'/>
         {/* title */}
@@ -28,10 +29,10 @@ export default function ProjectGallery() {
         <p className="project-card-date">Spring 2022</p>
         {/* description */}
         <p>Grounded by Linda Tuhiwai Smith’s definition of struggle (2012), this body of work evaluates the role of visual, verbal, and visceral storytelling in engaging the imagination as a language of possibility outside that of colonialism. It begs the question: how might we decolonize our minds, bodies, hearts, and spirits by (re-)engaging with and (re-)embodying the stories that we are involved within?</p>
-      </div>
+      </Link>
 
       {/* teaching portfolio card */}
-      <Link className="project-card" tabIndex={0} to="https://drive.google.com/file/d/1BhGsqbYhOBvw3Iwirc3qyxozu0knQWad/view?usp=sharing" target="_blank">
+      <Link className="project-card" to="https://drive.google.com/file/d/1BhGsqbYhOBvw3Iwirc3qyxozu0knQWad/view?usp=sharing" target="_blank">
         {/* image */}
         <img src={require("./thumbnails/teaching-card-thumbnail.png")} alt="" className='project-card-img'/>
         {/* title */}
@@ -43,7 +44,7 @@ export default function ProjectGallery() {
       </Link>
 
       {/* maharam card */}
-      <div className="project-card" tabIndex={0}>
+      <Link className="project-card">
         {/* image */}
         <img src={require("./thumbnails/maharam-card-thumbnail.png")} alt="" className='project-card-img'/>
         {/* title */}
@@ -52,10 +53,10 @@ export default function ProjectGallery() {
         <p className="project-card-date">Summer 2021</p>
         {/* description */}
         <p>Funded through the Maharam Fellowship Grant awarded by RISD, my teammate (Juliana) and I partnered with Groundwork Rhode Island's Green Team Youth to introduce and follow-through with youth-led projects aimed at identifying and solving small-scale issues around Providence and Pawtucket. In the end, the youth advertised their designs to the local community at two separate events.</p>
-      </div>
+      </Link>
 
       {/* growing system's tree card */}
-      <Link className="project-card" tabIndex={0} to="https://drive.google.com/file/d/1gcyQGz2V9W-rni94g3mNcIlSPbZCPAQl/view?usp=sharing" target="_blank">
+      <Link className="project-card" to="https://drive.google.com/file/d/1gcyQGz2V9W-rni94g3mNcIlSPbZCPAQl/view?usp=sharing" target="_blank">
         {/* image */}
         <img src={require("./thumbnails/tree-card-thumbnail.png")} alt="" className='project-card-img'/>
         {/* title */}
@@ -67,7 +68,7 @@ export default function ProjectGallery() {
       </Link>
 
       {/* weaste card */}
-      <div className="project-card" tabIndex={0}>
+      <Link className="project-card">
         {/* image */}
         <img src={require("./thumbnails/weaste-card-thumbnail.png")} alt="" className='project-card-img'/>
         {/* title */}
@@ -76,7 +77,7 @@ export default function ProjectGallery() {
         <p className="project-card-date">Spring 2021</p>
         {/* description */}
         <p>As commentary for the amount of waste produced during the design process during a core industrial design course, (we)aste consists of a trash can, skirt, and bag made of scavenged unwanted materials. It exists to raise awareness of unnecessary waste production oftentimes ignored throughout design, especially during the prototype phase.</p>
-      </div>
+      </Link>
     </div>
   </>);
 }
--- src/projects/ProjectPage.js
+++ src/projects/ProjectPage.js
@@ -4,11 +4,11 @@ import { Fade } from 'react-awesome-reveal';
 
 export default function ProjectPage() {
   return (<>
-    <Fade>
+    
       <div className="w-full flex flex-col justify-center items-center gap-10 p-4 py-10">
         <h2>projects</h2>
         <ProjectGallery />
       </div>
-    </Fade>
+    
   </>);
 }
