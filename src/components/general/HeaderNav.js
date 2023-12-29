026e7aaff837b8d6bdfded3a74d9e0c94b28d1e4
jsunvelascohebert <jsun.velasco.hebert@gmail.com>
2023-12-29 12:25:18 -0500

    add mobile menu

--- src/components/general/HeaderNav.js
+++ src/components/general/HeaderNav.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-import { IoMenu } from "react-icons/io5";
+import { IoMenu, IoClose } from "react-icons/io5";
 
 
 export default function HeaderNav() {
+
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (<>
     {/* desktop headernav */}
     <div className="hidden w-full sticky top-0 bg-purple md:flex flex-row justify-between items-center p-4 border-4 border-darkPurple z-50">
@@ -34,8 +37,9 @@ export default function HeaderNav() {
       </div>
     </div>
     
-    {/* mobile headernav */}
-    <div className="sticky top-0 w-full flex flex-row md:hidden justify-between items-center bg-purple p-3 border-4 border-darkPurple z-50">
+    {/* mobile header nav */}
+    {!isMobileMenuOpen &&
+      <div className="sticky top-0 w-full flex flex-row md:hidden justify-between items-center bg-purple p-3 border-4 border-darkPurple z-50">
       <Link to="/" className='flex flex-row gap-2 justify-center items-center group'>
         {/* logo */}
         <img src={require('./../assets/hero-logo_1.png')} alt="logo" className='max-h-[3vh] group-hover:scale-105' />
@@ -44,7 +48,52 @@ export default function HeaderNav() {
           jsun velasco-hébert
         </div>
       </Link>
-      <IoMenu className='text-darkPurple text-2xl'/>
+      <IoMenu className='text-darkPurple text-2xl' onClick={() => setMobileMenuOpen(true)} />
     </div>
+    }
+
+    {/* mobile menu expanded */}
+    {isMobileMenuOpen && 
+      <div id="mobileMenu" className="fixed top-0 left-0 right-0 z-50 w-full h-full overflow-hidden md:hidden bg-gradient-to-b from-purple to-white">
+        {/* close button */}
+        <div className="w-full flex flex-row justify-end items-center p-3 border-4 border-purple border-opacity-0">
+          <IoClose className='text-darkPurple text-2xl' onClick={() => setMobileMenuOpen(false)} />
+        </div>
+
+        {/* menu content */}
+        <div className="flex flex-col w-full h-[95vh] overflow-hidden justify-around items-center">
+
+          {/* name and logo */}
+          <Link className="flex flex-col justify-center items-center gap-2 hover:gap-3 transition-all group" onClick={() => setMobileMenuOpen(false)}>
+            <img src={require('./../assets/hero-logo_1.png')} alt='logo' className='max-w-[40vw] group-hover:scale-105 transition-all'/>
+            <h3 className='group-hover:scale-105 group-hover:underline transition-all'>jsun velasco-hébert</h3>
+          </Link>
+
+          {/* menu items */}
+          <div className="flex flex-col justify-center items-center gap-4">
+            <Link to="" className="menu-item-disabled">cv</Link>
+            <Link to='/about' className='menu-item'
+              onClick={() => setMobileMenuOpen(false)}>about</Link>
+            <Link to='/process' className="menu-item"
+              onClick={() => setMobileMenuOpen(false)}>process</Link>
+            <Link to='/projects' className="menu-item"
+              onClick={() => setMobileMenuOpen(false)}>projects</Link>
+          </div>
+
+          {/* external links */}
+          <div className="flex flex-row justify-center items-center gap-4">
+          <Link to="https://www.linkedin.com/in/jsunvelascohebert/" target='_blank'>
+            <FaLinkedin className='text-2xl text-darkPurple' />
+          </Link>
+          <Link to="https://github.com/jsunvelascohebert" target="_blank">
+            <FaGithubSquare className='text-2xl text-darkPurple' />
+          </Link>
+          </div>
+
+        </div>
+      </div>
+    }
+
   </>);
 }
+
